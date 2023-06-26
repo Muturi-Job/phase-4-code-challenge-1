77f01d76d213810c25c6bd2ada8d07f9e6866171
Job Muturi <muirimuturi17@gmail.com>
2023-06-26 11:57:23 +0300

    Updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# 5.times do 
+#     restaurant = Restaurant.create(name: Faker::Restaurant.name, address: Faker::Address.full_address)
+#     pizza = Pizza.create(name:Faker::Food.dish, ingredients: Faker::Food.ingredient)
+#     curr_price = (1..30).to_a.sample
+#     puts curr_price
+#     RestaurantPizza.create(price: curr_price, pizza_id: pizza.id, restaurant_id: restaurant.id)
+# end
+
+# Create some restaurants
+restaurant1 = Restaurant.create(name: "Pizza Hut", address: Faker::Address.full_address)
+restaurant2 = Restaurant.create(name: "Papa John's", address: Faker::Address.full_address)
+restaurant3 = Restaurant.create(name: "Domino's", address: Faker::Address.full_address)
+
+# Create some pizzas
+pizza1 = Pizza.create(name: "Pepperoni", ingredients: "Pepperoni, cheese, tomato sauce")
+pizza2 = Pizza.create(name: "Margherita", ingredients: "Fresh mozzarella cheese, fresh basil leaves, tomato sauce, olive oil")
+pizza3 = Pizza.create(name: "Supreme", ingredients:"Tomato sauce, mozzarella cheese, pepperoni, sausage, green peppers, onions, mushrooms, black olives")
+
+# Create some restaurant pizzas
+RestaurantPizza.create(price: 13, pizza_id: pizza1.id,  restaurant_id: restaurant1.id)
+RestaurantPizza.create(price: 7, pizza_id: pizza2.id, restaurant_id: restaurant1.id) 
+RestaurantPizza.create(price: 12, pizza_id: pizza2.id, restaurant_id: restaurant2.id)
+RestaurantPizza.create(price: 15, pizza_id: pizza3.id, restaurant_id: restaurant2.id)
+RestaurantPizza.create(price: 25, pizza_id: pizza1.id, restaurant_id: restaurant3.id)
+RestaurantPizza.create(price: 10, pizza_id: pizza3.id, restaurant_id: restaurant3.id)
