e9d227cc95c3309440e6f28835f55b70995fae95
Job Muturi <muirimuturi17@gmail.com>
2023-06-26 11:55:55 +0300

    Completed deliverables

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,2 +1,18 @@
 class RestaurantPizzasController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :restaurantpizza_invalid
+    
+    def create
+        restaurant = RestaurantPizza.create!(default_params)
+        pizza = restaurant.pizza
+        render json: pizza, status::created
+    end
+
+    private
+    def default_params
+        params.permit(:price, :pizza_id, :restaurant_id)
+    end
+
+    def restaurantpizza_invalid(invalid)
+        render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+    end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,7 @@
 class Restaurant < ApplicationRecord
-    has_many :restaurant_pizzas
+    has_many :restaurant_pizzas, dependent: :destroy
     has_many :pizzas, through: :restaurant_pizzas
 
-    
+
 
 end
